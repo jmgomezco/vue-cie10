3b999b3aa41638277d30c933522e75dd7b1b861e
jmgomezco <jmgomezco@gmail.com>
2025-10-18 16:35:00 +0200

    Adjust margin for ContainerInicial component

--- src/components/ContainerInicial.vue
+++ src/components/ContainerInicial.vue
@@ -17,7 +17,7 @@ export default {}
   width: 95%;
   max-width: 900px;
   min-height: 100vh;
-  margin: 40px auto 0 auto;
+  margin: 25px auto 0 auto;
   padding: 0 14px;
   box-sizing: border-box;
   font-family: monospace;
